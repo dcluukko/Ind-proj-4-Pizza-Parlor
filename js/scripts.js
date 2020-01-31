3b0c63fa8f5ed10e3b26b0fa5dad1c32c7811c62
Dana Luukko <Dcluukko@gmail.com>
2020-01-31 13:29:45 -0800

    working out issues in definitiion of .getPizza

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,6 @@ function Order() {
 
 Order.prototype.getPizza = function (size, toppings) {
 	var newPizza = new Pizza (size, toppings);
-	console.log(newPizza)
 	this.pizzaOrders.push(newPizza);
 }
 
@@ -17,16 +16,19 @@ function Pizza(size, toppings) {
 	this.cost = cost = []
 }
 //BL : Cost
-Pizza.prototype.calcTotalCost = function (size) {
-	var calcPizzaCost = this.size * (2) + this.toppings[0]
+Pizza.prototype.calcTotalCost = function () {
+	var calcPizzaCost = this.size * (2) + this.toppings[0].length
 	this.cost.push(parseInt(calcPizzaCost));
 }
 // User Interface Logic
 
 var order = new Order();
 function displayToppingList(pizza){
-	var htmlForToppinfList = "";
-		pizza.forEach(function(topping))
+	var htmlForToppingList = "";
+		pizza.forEach(function(topping) {
+			htmlForToppingList += "<li>" + topping + "</li>"
+		});
+		$("#show-pizza-topping").html(htmlForToppingList);
 }
 
 function createNewPizza(size, toppings){
@@ -38,14 +40,13 @@ $(document).ready(function () {
 	var toppingInput = [];
 	$("form#select-pizza-size-topping").submit(function () {
 		event.preventDefault();
-		var selectedSize = parseInt($("#pizza-size").val());
-		console.log(selectedSize);
+		var sizeInput = parseInt($("#pizza-size").val());
+		
 		$("input:checkbox[name=topping]:checked").each(function () {
 			var pizzaToppingsChosen = $(this).val();
 			toppingInput.push(pizzaToppingsChosen);
-			console.log(pizzaToppingsChosen);
 		});
-		getPizza(selectedSize,toppingInput);
-		
+		createNewPizza(sizeInput,toppingInput);
+		displayToppingList(order.pizzas[0].toppings[0]);
 	});
 });
