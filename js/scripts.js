875f22b2caf8c04613eab5be9109b84bf1b604c3
Dana Luukko <Dcluukko@gmail.com>
2020-01-31 08:38:50 -0800

    added HTML title

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+//Pizza Logic
+function Pizza() {
+	this.toppings = [],
+		this.currentId = 0
+}
+
+//Topping Logic
+function Topping(soyCurls, broccoli, spinach, nutritionalYeast) {
+	this.soyCurls = soyCurls,
+	this.broccoli = broccoli,
+	this.spinach = spinach,
+	this.nutritionalYeast = nutritionalYeast
+}
+
+//Pricing Logic
+
+
+
+
+
+
+
+
+
+
+
+
+// User Interface Logic
+$(document).ready(function () {
+	event.preventDefault() {
+
+	}
+});
