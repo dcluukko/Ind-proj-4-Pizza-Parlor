0be1330184bdb3953e50dfe1f51bb31bab79c95b
Dana Luukko <dcluukko@gmail.com>
2020-02-01 08:31:50 -0800

    created a new pizza variable that allows me to add both size and toppings to var new pizza

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,14 @@ function Order() {
 }
 
 Order.prototype.getPizza = function (size, toppings) {
-	var newPizza = new Pizza (size, toppings);
+	var newPizza = new Pizza(size, toppings);
 	this.pizzaOrders.push(newPizza);
 }
 
 //BL : Pizza Logic
 function Pizza(size, toppings) {
 	this.size = size,
-	this.toppings = [],
+		this.toppings = [],
 		this.toppings.push(toppings);
 	this.cost = cost = []
 }
@@ -26,32 +26,32 @@ Pizza.prototype.calcTotalCost = function () {
 
 var order = new Order();
 //Loop to Display Toppings List
-function displayToppingList(pizza){
+function displayToppingList(pizza) {
 	var htmlForToppingList = "";
-		pizza.forEach(function(topping) {
-			htmlForToppingList += "<li>" + topping + "</li>"
-		});
-		$("#show-pizza-topping").html(htmlForToppingList);
+	pizza.map(function (topping) {
+		htmlForToppingList += "<li>" + topping + "</li>"
+	});
+	$("#show-pizza-topping").html(htmlForToppingList);
 }
 // Collecting Order Information 
-function createNewPizza(size, toppings){
+function createNewPizza(size, toppings) {
 	order.getPizza(size, toppings);
 	order.pizzaOrders[0].calcTotalCost(order.pizzaOrders[0].size);
 
-		$("#show-pizza-size").html(order.pizzaOrders[0].size);
-		$("#total-cost").html(order.pizzaOrders[0].cost);
+	$("#show-pizza-size").html(order.pizzaOrders[0].size);
+	$("#total-cost").html(order.pizzaOrders[0].cost);
 }
 $(document).ready(function () {
 	var toppingInput = [];
 	$("form#select-pizza-size-topping").submit(function () {
 		event.preventDefault();
 		var sizeInput = parseInt($("#pizza-size").val());
-		
+
 		$("input:checkbox[name=topping]:checked").each(function () {
 			var pizzaToppingsChosen = $(this).val();
 			toppingInput.push(pizzaToppingsChosen);
 		});
-		createNewPizza(sizeInput,toppingInput);
+		createNewPizza(sizeInput, toppingInput);
 		displayToppingList(order.pizzaOrders[0].toppings[0]);
 		$()
 		$(".whole-form").hide();
